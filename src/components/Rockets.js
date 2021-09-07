808f898cdd6419410076178c812253d539cba0a9
Breno Xavier <brenoxav@gmail.com>
2021-09-07 18:22:57 -0400

    Install React-Bootstrap, style Rockets page

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
 export default function Rockets() {
   const rockets = useSelector((state) => state.rockets);
   const rocketList = rockets.map((rocket) => (
-    <article key={rocket.id}>
-      <img src={rocket.flickr_images[0]} alt={rocket.rocket_name} />
-      <h2>{rocket.rocket_name}</h2>
-      <p>{rocket.description}</p>
-    </article>
+    <Row key={rocket.id} className="my-4">
+      <Col sm={3}>
+        <Image src={rocket.flickr_images[0]} alt={rocket.rocket_name} fluid />
+      </Col>
+      <Col>
+        <h2 className="h4">{rocket.rocket_name}</h2>
+        <p>{rocket.description}</p>
+        <Button variant="primary">Reserve Rocket</Button>
+      </Col>
+    </Row>
   ));
 
   return (
-    <main>
-      <h1>Rockets</h1>
+    <Container class="container-xl">
       {rocketList}
-    </main>
+    </Container>
   );
 }
