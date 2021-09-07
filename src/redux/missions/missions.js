215896d8714d18ea6ff58ab0197587199ea1ab81
Ijay Abby <inyangasi4@gmail.com>
2021-09-08 02:01:45 +0300

    Updtate missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,40 +1,40 @@
 import api from '../../api/SpaceXAPI';
 
-const getMissionsFromApi = 'missions/getMissionsFromApi';
+const GET_MISSIONS_FROM_API = 'missions/GET_MISSIONS_FROM_API';
 
-//Initial state
+// Initial state
 
 const initialState = [];
 
 const missionsData = (apiResponse) => {
-    const missionsArray = apiResponse.map((mission) => ({
-        id:mission.id,
-        mission_name:mission.mission_name,
-        description:mission.description,
-    }));
-    return missionsData;
+  const missionsArray = apiResponse.map((mission) => ({
+    id: mission.id,
+    mission_name: mission.mission_name,
+    description: mission.description,
+  }));
+  return missionsArray;
 };
 
 export const getMissionsFromApi = () => ((dispatch) => {
-    api.getMisiions()
+  api.getMissions()
     .then((response) => {
-        const payload = missionsData(response);
-        dispatch({
-            type: getMissionsFromApi,
-            payload,
-        });
+      const payload = missionsData(response);
+      dispatch({
+        type: GET_MISSIONS_FROM_API,
+        payload,
+      });
     });
 });
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case getMissionsFromApi:
-            return [
-                ...action.payload,
-            ];
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case GET_MISSIONS_FROM_API:
+      return [
+        ...action.payload,
+      ];
+    default:
+      return state;
+  }
 };
 
 export default reducer;
