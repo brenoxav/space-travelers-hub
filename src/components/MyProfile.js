77248091e9708206314d5a561e9d29eb4089b7aa
Breno Xavier <brenoxav@gmail.com>
2021-09-08 16:31:38 -0400

    Render a list of all reserved rockets on the 'My Profile' page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function MyProfile() {
+  const rockets = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
+  const myRockets = reservedRockets.map((rocket) => (
+    <ListGroup.Item key={rocket.id}>
+      {rocket.rocket_name}
+    </ListGroup.Item>
+  ));
+
+  const myMissions = [];
+
   return (
-    <div>
-      <h1>My Profile</h1>
-    </div>
+    <Container className="container-xl">
+      <Row className="mt-4">
+        <Col>
+          <h2>My Missions</h2>
+          <ListGroup>
+            {myMissions}
+          </ListGroup>
+        </Col>
+
+        <Col>
+          <h2>My Rockets</h2>
+          <ListGroup>
+            {myRockets}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
