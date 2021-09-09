cc79a3d75e2b8fa07fdb1219d5ac25056f1b2baa
Ijay Abby <inyangasi4@gmail.com>
2021-09-09 22:55:36 +0300

    Implement mission actions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -8,21 +8,53 @@ import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Badge from 'react-bootstrap/Badge';
 
+import store from '../redux/configureStore';
+import { reserveMission, cancelMissionReservation } from '../redux/missions/missions';
+
 export default function Missions() {
-  // get missions data from the store
   const missions = useSelector((state) => state.missions);
+
+  const handleClick = (mission) => (e) => {
+    if (e.target.textContent === 'Join Mission') {
+      store.dispatch(reserveMission(mission));
+    } else if (e.target.textContent === 'Leave Mission') {
+      store.dispatch(cancelMissionReservation(mission));
+    }
+  };
 
   const missionList = missions.map((mission) => (
     <tr key={mission.id}>
-      <td>{mission.mission_name}</td>
+      <th className="align-middle">{mission.mission_name}</th>
       <td>{mission.description}</td>
-      <td>
-        <Badge bg="success" className="me-2">
-          Active Member
-        </Badge>
+      <td className="align-middle">
+        {!mission.reserved && (
+          <Badge bg="secondary" className="me-2">Not a member</Badge>
+        )}
+
+        {mission.reserved && (
+          <Badge bg="success" className="me-2">Active Member</Badge>
+        )}
       </td>
-      <td>
-        <Button variant="outline-secondary">Join Mission</Button>
+      <td className="align-middle">
+        {!mission.reserved && (
+          <Button
+            className="text-nowrap"
+            variant="outline-secondary"
+            onClick={handleClick(mission)}
+          >
+            Join Mission
+          </Button>
+        )}
+
+        {mission.reserved && (
+          <Button
+            className="text-nowrap"
+            variant="outline-danger"
+            onClick={handleClick(mission)}
+          >
+            Leave Mission
+          </Button>
+        )}
       </td>
     </tr>
   ));
